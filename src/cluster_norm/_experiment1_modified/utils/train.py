e7c009ade244c1c5cac5f797d15ed157b89242c1
ThatAura <116550397+ThatAura@users.noreply.github.com>
2025-04-09 16:31:51 +0200

    a.colacicco - refactor

--- src/cluster_norm/_experiment1_modified/utils/train.py
+++ src/cluster_norm/_experiment1_modified/utils/train.py
@@ -6,8 +6,8 @@ from jaxtyping import Float
 from sklearn.linear_model import LogisticRegression
 from torch import Tensor
 
-from cluster_norm._experiment1_modified.utils.CCS import CCS
-from cluster_norm._experiment1_modified.utils.CRC import CRC
+from CCS import CCS
+from CRC import CRC
 
 
 def fit_logreg(
